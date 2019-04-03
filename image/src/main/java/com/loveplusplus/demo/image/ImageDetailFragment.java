0d99bb35d20b747b1061ea7332990a44f8383cc9
sun <1551878404@qq.com>
2019-04-03 12:20:30 +0800

    修改

--- image/src/main/java/com/loveplusplus/demo/image/ImageDetailFragment.java
+++ image/src/main/java/com/loveplusplus/demo/image/ImageDetailFragment.java
@@ -49,7 +49,7 @@ public class ImageDetailFragment extends Fragment {
 			
 			@Override
 			public void onPhotoTap(View arg0, float arg1, float arg2) {
-				getActivity().finish();
+//				getActivity().finish();
 			}
 		});
 		
