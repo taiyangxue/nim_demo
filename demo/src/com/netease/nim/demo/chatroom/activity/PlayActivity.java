2a42680e911392cfe095621951e987fd5f2b1cdb
user <1551878404@qq.com>
2019-04-21 00:11:55 +0800

    终版

---
+++ demo/src/com/netease/nim/demo/chatroom/activity/PlayActivity.java
@@ -0,0 +1,15 @@
+package com.netease.nim.demo.chatroom.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.netease.nim.demo.R;
+
+public class PlayActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+    }
+}
